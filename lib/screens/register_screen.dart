9b131b93f76656a744ed5acc2a673ebc19212aee
Your Name <you@example.com>
2023-12-15 13:18:19 +0300

    app

---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,145 @@
+import 'package:country_picker/country_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:guzzler/widgets/custom_button.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController phoneController = TextEditingController();
+
+  Country selectedCountry = Country(
+    phoneCode: "254",
+    countryCode: "ke",
+    e164Sc: 0,
+    geographic: true,
+    level: 1,
+    name: "Kenya",
+    example: "Kenya",
+    displayName: "Kenya",
+    displayNameNoCountryCode: "KE",
+    e164Key: "",
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    phoneController.selection = TextSelection.fromPosition(
+      TextPosition(
+        offset: phoneController.text.length,
+      ),
+    );
+    return Scaffold(
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Center(
+            child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+                child: Column(
+                  children: [
+                    Container(
+                      width: 200,
+                      height: 200,
+                      padding: const EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.red.shade50,
+                      ),
+                      child: Image.asset(
+                        "assets/image1.png",
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Register",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      "Add your phone number,We'll send you a verification code",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black38,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      cursorColor: Colors.red,
+                      controller: phoneController,
+                      onChanged: (value) {
+                        setState(() {
+                          phoneController.text = value;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        hintText: "Enter Phone Number",
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(color: Colors.black12),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(color: Colors.black12),
+                        ),
+                        prefixIcon: Container(
+                          padding: const EdgeInsets.all(8.0),
+                          child: InkWell(
+                            onTap: () {
+                              showCountryPicker(
+                                  context: context,
+                                  countryListTheme: const CountryListThemeData(
+                                      bottomSheetHeight: 550),
+                                  onSelect: (value) {
+                                    setState(() {
+                                      selectedCountry = value;
+                                    });
+                                  });
+                            },
+                            child: Text(
+                              "${selectedCountry.flagEmoji} ${selectedCountry.phoneCode}",
+                              style: const TextStyle(
+                                fontSize: 18,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        suffixIcon: phoneController.text.length > 8
+                            ? Container(
+                                height: 30,
+                                width: 30,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.green,
+                                ),
+                                child: const Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                  size: 20,
+                                ),
+                              )
+                            : null,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: CustomButton(text: "Login", onPressed: () {}),
+                    )
+                  ],
+                ))),
+      ),
+    );
+  }
+}
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:guzzler/provider/auth_provider.dart';
+import 'package:guzzler/screens/home_screen.dart';
+import 'package:guzzler/screens/register_screen.dart';
+import 'package:guzzler/widgets/custom_button.dart';
+import 'package:provider/provider.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final ap = Provider.of<AuthProvider>(context, listen: false);
+
+    return Scaffold(
+        body: SafeArea(
+            child: Center(
+                child: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/welcome.png",
+            height: 300,
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            "FAST DISPATCH",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            "Let the drinks come to you!",
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.black38,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20),
+          //custom button
+          SizedBox(
+            width: double.infinity,
+            height: 50,
+            child: CustomButton(
+              onPressed: () {
+                ap.isSignedIn == false
+                    ? Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomeScreen()))
+                    : Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const RegisterScreen(),
+                        ),
+                      );
+              },
+              text: "Get Started",
+            ),
+          )
+        ],
+      ),
+    ))) //
+        );
+  }
+}
